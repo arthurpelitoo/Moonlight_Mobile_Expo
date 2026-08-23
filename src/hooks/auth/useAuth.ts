d4ea22041a7eafd74713074ec7761a4cd9243666
arthurpelitoo <arthurantoniocontact@gmail.com>
2026-08-23 00:52:16 -0300

    feat(contexts): adicionado contexts polir melhor com o tempo

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,15 +1,15 @@
-// import { createContext, useContext } from "react";
-// import type { AuthUserResponseDTO } from "../../@types/auth/auth.dto";
+import { createContext, useContext } from "react";
+import type { AuthUserResponseDTO } from "../../@types/auth/auth.dto";
 
-// export type AuthContextType = {
-//     user: AuthUserResponseDTO | null;
-//     token: string | null;
-//     login: (token: string, user: AuthUserResponseDTO) => void;
-//     logout: () => void;
-//     isAuthenticated: boolean;
-//     loading: boolean;
-// }
+export type AuthContextType = {
+    user: AuthUserResponseDTO | null;
+    token: string | null;
+    login: (token: string, user: AuthUserResponseDTO) => void;
+    logout: () => void;
+    isAuthenticated: boolean;
+    loading: boolean;
+}
 
-// export const AuthContext = createContext({} as AuthContextType);
+export const AuthContext = createContext({} as AuthContextType);
 
-// export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
